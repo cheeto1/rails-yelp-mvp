ec15071d78ed5a523a042e496170dc774a2424c7
Che To <chekinto@hotmail.com>
2019-05-13 14:49:21 +0100

    Yelp MVC completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0208 534 8753',
+    category:     'chinese',
+  },
+  {
+    name:         'KFC',
+    address:      '12 Longlane St, London F5 GH6',
+    phone_number: '0208 128 9368',
+    category:     'belgian',
+  },
+  {
+    name:         'Pizza Hut',
+    address:      '101 Pizza Lane, London P1 ZAP',
+    phone_number: '0208 026 3057',
+    category:     'japanese',
+  },
+  {
+    name:         'Pasta City',
+    address:      '101 Happy St, London H7 G76',
+    phone_number: '0208 026 3057',
+    category:     'italian',
+  },
+  {
+    name:         'Baguette Club',
+    address:      '101 Rue Boulangerie, London P1 ZAP',
+    phone_number: '0208 026 3057',
+    category:     'french',
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
